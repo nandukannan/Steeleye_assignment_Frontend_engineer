4a7c384d9827ac17c52e9f72fbd3cc206783d8fa
Nandu kannan <50976992+nandukannan@users.noreply.github.com>
2022-09-30 19:26:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+
+
+import React, { useState, useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
+
+// Single List Item
+const WrappedSingleListItem = ({
+  index,
+  isSelected,
+  onClickHandler,
+  text,
+}) => {
+  return (
+    <li>
+      style={{ backgroundColor: isSelected ? 'green' : 'red'}}
+      <button onClick={onClickHandler(index)} >+</button>
+    
+      {text}
+    </li>
+  );
+};
+
+WrappedSingleListItem.propTypes = {
+  index: PropTypes.number,
+  isSelected: PropTypes.bool,
+  onClickHandler: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+const SingleListItem = memo(WrappedSingleListItem);
+//export default SingleListItem;
+//setSelectedIndex(() 
+//const  setSelectedIndex = index 
+// List Component
+const WrappedListComponent = ({
+  items,
+}) => {
+  const [setSelectedIndex, selectedIndex] = useState();
+
+    useEffect(() => {
+        //renamed fucntion name from setSelectedIndex to selectedIndex
+    selectedIndex(null);
+  }, [items,setSelectedIndex]);
+
+  const handleClick = index => {
+    selectedIndex(index);
+  };
+
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      {
+      items.map((item, index) => (
+        <SingleListItem
+        key={index}
+        onClickHandler={() => handleClick(index)}
+          text={item.text}
+          index={index}
+          isSelected={setSelectedIndex}
+        />
+      ))}
+    </ul>
+  )
+};
+
+WrappedListComponent.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string.isRequired,
+  })),
+};
+
+WrappedListComponent.defaultProps = {
+  items: null,
+};
+
+const List = memo(WrappedListComponent);
+
+export default List;
+
